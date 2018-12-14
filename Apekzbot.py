18b2433cc586c92ce8e086674519cf5fe91da5f5
eAzTea <45879371+eAzTea@users.noreply.github.com>
2018-12-14 16:56:42 +0100

    Update Apekzbot.py

--- Apekzbot.py
+++ Apekzbot.py
@@ -15,7 +15,6 @@ Clientdiscord = discord.Client()
 @client.event
 async def on_member_join(member):
     print('Recognised that a member called ' + member.name + ' joined')
-    await client.send_message(member, 'Willkommen beim ApekZ Clan! Melde dich gleich bei einen Operator, dieser wird dir dann alles erklären. Wenn keiner online ist dann melde dich bei jemanden von der Leitung! Wir wünschen dir VIEL SPAß ')
     print('Sent message to ' + member.name)
 async def on_ready():
     await client.change_presence(game=Game(name='Willkommen in Apekz Clan'))
